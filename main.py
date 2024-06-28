f2a4f0d4b5eab129208058367db5310f93db312f
nk125 <nekes125@outlook.com>
2024-06-27 22:26:17 -0600

    feat: Controller keep track when key is pressed

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ screen.fill((0, 0, 0))
 
 pygame.display.set_caption("ContaEscape")
 
+pygame.display.flip()
+
+controller.update(controller.states.NONE)
+
 running = True
 while running:
 	screen.blit(controller.surf, controller.rect)
@@ -19,24 +23,23 @@ while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
-		
-		if event.type == pygame.KEYDOWN:
-			keyPressed = event.key
-			
-			if keyPressed == pygame.K_DOWN:
-				controller.update(controller.states.DOWN)
-
-			elif keyPressed == pygame.K_RIGHT:
-				controller.update(controller.states.RIGHT)
-
-			elif keyPressed == pygame.K_UP:
-				controller.update(controller.states.UP)
-
-			elif keyPressed == pygame.K_LEFT:
-				controller.update(controller.states.LEFT)
 
 		if event.type == Constants.RESET_CONTROLLER:
 			controller.update(controller.states.NONE)
+
+	pressed_keys = pygame.key.get_pressed()
+
+	if pressed_keys[pygame.K_DOWN]:
+		controller.update(controller.states.DOWN)
+
+	if pressed_keys[pygame.K_RIGHT]:
+		controller.update(controller.states.RIGHT)
+
+	if pressed_keys[pygame.K_UP]:
+		controller.update(controller.states.UP)
+		
+	if pressed_keys[pygame.K_LEFT]:
+		controller.update(controller.states.LEFT)
 
 	pygame.display.update()
 
